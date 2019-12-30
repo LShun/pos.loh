6c970dc654319b4650cda9a2e76d2a56e74a0a9d
equinox.bot <hiewlshun@gmail.com>
2019-12-30 10:35:41 +0800

    Change make.bat to make for correct.asm

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
-masm code\LOHSYS~1 code\LOHSYS~1;
-link code\LOHSYS~1;
-copy LOHSYS~1.exe code\LOHSYS~1.exe
-del LOHSYS~1.exe
-code\LOHSYS~1.exe
+masm code\CORRECT code\CORRECT;
+link code\CORRECT;
+copy CORRECT.exe code\CORRECT.exe
+del CORRECT.exe
+code\CORRECT.exe
